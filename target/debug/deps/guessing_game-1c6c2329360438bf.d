fdd48779b1e2dbf07f9a82d1879888831527eef0
Parjolu <fatbotul@gmail.com>
2024-04-06 22:00:20 +0300

    first commit

---
+++ target/debug/deps/guessing_game-1c6c2329360438bf.d
@@ -0,0 +1,5 @@
+/home/parjol/Documents/Rust/guessing_game/target/debug/deps/libguessing_game-1c6c2329360438bf.rmeta: src/main.rs
+
+/home/parjol/Documents/Rust/guessing_game/target/debug/deps/guessing_game-1c6c2329360438bf.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/parjol/Documents/Rust/guessing_game/target/debug/guessing_game: /home/parjol/Documents/Rust/guessing_game/src/main.rs
