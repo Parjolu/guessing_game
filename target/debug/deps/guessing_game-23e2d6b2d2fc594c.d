5b2fc6c3b0c7595a3ed98e42037fcaa35a7fb70f
Parjol <fatbotul@gmail.com>
2024-04-15 21:15:37 +0300

    yes

---
+++ target/debug/deps/guessing_game-23e2d6b2d2fc594c.d
@@ -0,0 +1,9 @@
+/home/parjol/Documents/Rust/guessing_game/target/debug/deps/libguessing_game-23e2d6b2d2fc594c.rmeta: src/main.rs Cargo.toml
+
+/home/parjol/Documents/Rust/guessing_game/target/debug/deps/guessing_game-23e2d6b2d2fc594c.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/deps/guessing_game-4dc5327ae9cfc259.d
@@ -0,0 +1,9 @@
+/home/parjol/Documents/Rust/guessing_game/target/debug/deps/libguessing_game-4dc5327ae9cfc259.rmeta: src/main.rs Cargo.toml
+
+/home/parjol/Documents/Rust/guessing_game/target/debug/deps/guessing_game-4dc5327ae9cfc259.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
